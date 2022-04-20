91b20d24eb01986fa8fef43932ed7d87a3fe710c
Longinus44 <chijiokelonginus4@gmail.com>
2022-04-20 23:48:41 +0100

    my first trial

---
+++ web.js
@@ -0,0 +1,15 @@
+/*let btn = document. getElementById(" my button");
+btn.onclick = hello;*/
+
+// window.onload = function() {
+//     let btn = document.getElementById("mybutton");
+//     btn.onclick = alert('HELLO');
+// };
+/*
+btn.onclick = function(){
+    console.log("hello");
+};*/
+
+function doSomething(){
+    alert('Hello Alex');
+}
